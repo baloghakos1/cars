02cba3f7e343077fc2014f7d307c17cb4914809c
3041TAN-19 <3041TAN-19@boronkay.local>
2023-12-04 11:51:58 +0100

    1151

--- db-tools.php
+++ db-tools.php
@@ -1,17 +1,17 @@
 <?php
 function insertMakers($mysqli, $makers, $truncate = false) {
     if($truncate) {
-        $mysqli->query("TRUNCATE TABLE makers");
+        $mysqli->query("TRUNCATE TABLE {self::DBTABLE};");
     }
     foreach ($makers as $maker) {
-        $result = $mysqli->query("INSERT INTO makers (name) Values ('$maker')");
+        $result = $mysqli->query("INSERT INTO {self::DBTABLE} (name) Values ('$maker')");
     }
     return $result;
 }
 
 function updateMakers($mysqli, $data) {
     $makerName = $data['name'];
-    $result = $mysqli->query("UPDATE makers SET name=$makerName");
+    $result = $mysqli->query("UPDATE {self::DBTABLE} SET name=$makerName");
 
     if (!$result) {
         echo "Hiba történt a $makerName beszúrása közben";
@@ -24,27 +24,27 @@ function updateMakers($mysqli, $data) {
 }
 
 function getMaker($mysqli, $id) {
-    $result = $mysqli->query("SELECT * FROM makers Where id=$id");
+    $result = $mysqli->query("SELECT * FROM {self::DBTABLE} Where id=$id;");
     $maker = $result->fetch_assoc();
 
     return $maker;
 }
 
 function getMakerByName($mysqli, $name) {
-    $result = $mysqli->query("SELECT * FROM makers Where name=$name");
+    $result = $mysqli->query("SELECT * FROM {self::DBTABLE} Where name=$name;");
     $maker = $result->fetch_assoc();
 
     return $maker;
 }
 
 function delMaker($mysqli, $id) {
-    $result = $mysqli->query("DELETE makers Where id=$id");
+    $result = $mysqli->query("DELETE {self::DBTABLE} Where id=$id;");
 
     return $result;
 }
 
 function getAllMakers($mysqli) {
-    $result = $mysqli->query("SELECT * FROM makers");
+    $result = $mysqli->query("SELECT * FROM {self::DBTABLE};");
     $makers = $result->fetch_all(MYSQLI_ASSOC);
     $result->free_result();
 
